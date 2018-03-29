e4d73e51a40e2b506a64a16e2b0bbccef4792133
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-03-29 21:32:37 +0200

    Added JS that adds css classes as you scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,28 @@
 {
   const init = () => {
+    window.addEventListener(`scroll`, () => {
+      let scrolled = window.pageYOffset;
+      let win_height_padded = window.innerHeight * 1.1;
 
+      document
+        .querySelectorAll(`.revealOnScroll:not(.animated)`)
+        .forEach(element => {
+          let offsetTop = element.offsetTop;
+
+          if (scrolled + win_height_padded > offsetTop) {
+            if (element.dataset.timeout) {
+              window.setTimeout(() => {
+                element.classList.add(
+                  `animated`,
+                  `${element.dataset.animation}`
+                );
+              }, parseInt(element.dataset.timeout, 10));
+            } else {
+              element.classList.add(`animated`, `${element.dataset.animation}`);
+            }
+          }
+        });
+    });
   };
 
   init();
